d94877e5c12d557f2617386f56c9414dcdf8b434
TopScrech <89252798+TopScrech@users.noreply.github.com>
2026-09-20 00:25:51 +0200

    Added visionOS icon & split visionOS into a separate target

---
+++ visionOS/SFTP_OtterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFTP_OtterApp.swift
+//  SFTP-Otter
+//
+//  Created by Sergei Saliukov on 20/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SFTP_OtterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
